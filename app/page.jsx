9b8d30344a38522a3a5aea7ff4c7f97dadc6103e
quang <ltb01628645546@gmail.com>
2023-07-20 14:44:33 +0700

    done

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import TopicsList from "@/app/components/TopicsList";
+
+export default function Home() {
+    return (
+        <>
+            <TopicsList/>
+        </>
+    )
+}
